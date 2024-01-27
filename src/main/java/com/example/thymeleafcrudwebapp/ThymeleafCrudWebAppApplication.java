48de019e818221d0a541c6505bf9ad9afe09cf21
Your Name <you@example.com>
2024-01-28 01:44:18 +0300

    First commit

---
+++ src/main/java/com/example/thymeleafcrudwebapp/ThymeleafCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafcrudwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafCrudWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/thymeleafcrudwebapp/ThymeleafCrudWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleafcrudwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafCrudWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
